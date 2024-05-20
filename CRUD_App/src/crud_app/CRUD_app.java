87cb433a897c84de48094a5ea02d2f9cd7f6def0
iqbal.bs_ <119472842+KocengHitam@users.noreply.github.com>
2024-05-20 12:43:05 +0700

    testtt

---
+++ CRUD_App/src/crud_app/CRUD_app.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package crud_app;
+
+/**
+ *
+ * @author Iqbal
+ */
+
+import com.login.LoginFrame;
+public class CRUD_app {
+
+    
+    public static void main(String args[]) {
+        LoginFrame loginObj = new LoginFrame();
+        loginObj.show();
+    }
+}
